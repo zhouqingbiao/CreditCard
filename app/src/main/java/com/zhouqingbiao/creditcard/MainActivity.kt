7d5f2ca21a72472cdf23dcba174eefaab251de77
Zhou <zhouqingbiao@qq.com>
2020-08-01 14:15:50 +0800

    Init

--- app/src/main/java/com/zhouqingbiao/creditcard/MainActivity.kt
+++ app/src/main/java/com/zhouqingbiao/creditcard/MainActivity.kt
@@ -14,15 +14,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         // 银行
-        val banks = mapOf(0 to "中国农业银行", 1 to "中国银行", 2 to "中国工商银行", 3 to "中国邮政储蓄银行")
+        val banks = mapOf(1 to "中国农业银行", 2 to "中国银行", 3 to "中国工商银行", 0 to "中国邮政储蓄银行")
 
         // 日历
         var calendar: Calendar = Calendar.getInstance()
-
+        var year = calendar.get(Calendar.YEAR)
+        var month = calendar.get(Calendar.MONTH) + 1
         var dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH)
+        var dayOfYear = calendar.get(Calendar.DAY_OF_YEAR)
 
-        var bank = findViewById<TextView>(R.id.bank)
-
-        bank.text = banks[dayOfMonth % banks.size]
+        findViewById<TextView>(R.id.date).text =
+            year.toString() + "年" + month.toString() + "月" + dayOfMonth.toString() + "日"
+        findViewById<TextView>(R.id.dayOfYear).text = dayOfYear.toString()
+        findViewById<TextView>(R.id.bank).text = banks[dayOfYear % banks.size]
     }
 }
